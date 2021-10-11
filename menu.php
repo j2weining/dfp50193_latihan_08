b28a4baf054c7868b04f4604b77a308ae2f5fe01
CHAI WEI NING <weining2001@gmail.com>
2021-10-11 11:48:53 +0800

    array

--- menu.php
+++ menu.php
@@ -3,4 +3,5 @@
 [
 <a href="index.php">numeric array</a>
 <a href="assoc.php">associative array</a>
-<a href="multi.php">multidimensional array</a>]
+<a href="multi.php">multidimensional array</a>
+]
--- multi.php
+++ multi.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php require 'menu.php'; ?>
     <h2>MULTIDIMENSIONAL ARRAY</h2>
 
     <?php
-    $perserta = [
-        'ali'=> [
-            'nama'=>'Ghazali',
-            'umur'=>16,
-            'jantina'=> 'Lelaki',
+    $peserta = [
+        'ali' => [
+            'nama' => 'Ghazali',
+            'umur' => 16,
+            'jantina' => 'Lelaki',
         ],
-        'adi'=>[
-            'nama'=>'Norhadi',
-            'umur'=>26,
-            'jantina'=> 'Lelaki',
+        'adi' => [
+            'nama' => 'Norhadi',
+            'umur' => 26,
+            'jantina' => 'Lelaki',
         ]
     ];
-        ?>
+    ?>
     <table border="1" cellspacing="0">
         <tr>
             <th>Bil</th>
@@ -33,10 +35,18 @@
         </tr>
         <?php
         $bil = 1;
-        foreach ($peserta as $pelajar){
-
+        foreach($peserta as $pelajar) {
+        ?>
+            <tr>
+                <td><?php echo $bil++; ?></td>
+                <td><?php echo $pelajar['nama']; ?></td>
+                <td><?php echo $pelajar['umur']; ?>tahun</td>
+                <td><?php echo $pelajar['jantina']; ?></td>
+            </tr>
+        <?php
         }
         ?>
     </table>
 </body>
+
 </html>
